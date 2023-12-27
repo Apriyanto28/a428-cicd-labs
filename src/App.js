1fdd7079e47d982666f95e55fce0ef4b4322ae5d
Apriyanto28 <apriyanto.halim@mikroskil.ac.id>
2023-12-27 19:18:43 +0700

    Melakukan perubahan pada file App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome My First Page Apriyanto Halim</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Ini merupakan tampilan utama dari website yang akan kalian lihat pada web ini</p>
+        <p>Tujuan dari website ini hanya untuk sebatas pembelajaran dan pemahaman pengujian</p>
+        <p>menggunakan blueocean.</p>
+        <p>Bagian yang diatas ditambahkan ke dalam tampilan utama. ^^^</p>
       </div>
     );
   }
